231baffb5499387968f6f502908bc1bcdb82f2ff
tianpeng <tzhang@g.harvard.edu>
2020-07-27 13:38:02 -0400

    remove coef publishing from sensor publishing script

--- src/PYTHON3_publish_all_sensors.py
+++ src/PYTHON3_publish_all_sensors.py
@@ -12,9 +12,6 @@ class sensor():
 	def __init__(self):
 		self.node = rospy.init_node("light_sensors", anonymous=True)
 		self.sensor_publisher = rospy.Publisher("sensor_readings", Float32MultiArray, queue_size=10)
-		self.coefs_publisher = rospy.Publisher("sensor_coefs", Float32MultiArray, queue_size=10)
-		self.coefs=Float32MultiArray()
-		self.coefs.data=np.loadtxt('{}/coefs_{}.txt'.format(os.environ['HOME'],socket.gethostname()),delimiter=',')
 		self.sensor_readings = Float32MultiArray()
    # Create I2C bus as normal
 		self.i2c = busio.I2C(board.SCL, board.SDA)
@@ -33,7 +30,6 @@ class sensor():
 				#print(e)
 			print(self.sensor_readings.data)
 			self.sensor_publisher.publish(self.sensor_readings)
-			self.coefs_publisher.publish(self.coefs)
 			r.sleep()
 
 if __name__ == '__main__':
