55fd0e038ce830c793486145a438500ada428581
Tianpeng Zhang <tzhang@g.harvard.edu>
2020-08-15 18:04:01 -0400

    Add ValueError catching in read all sensor script.

--- src/PYTHON3_read_all_sensors.py
+++ src/PYTHON3_read_all_sensors.py
@@ -5,24 +5,17 @@ import busio
 import adafruit_tsl2591
 import adafruit_tca9548a
 
+
 def read_all_sensors(tca):
-	# Create I2C bus as normal
-#	i2c = busio.I2C(board.SCL, board.SDA)
+	sensor_reading = []
+	for i in range(8):
+		try:
+			tsl=adafruit_tsl2591.TSL2591(tca[i])
+			sensor_reading.append(tsl.lux)
+		except ValueError:
+			pass
+			# ValueError arises normally the sensor is missing at that position.
 
-	# Create the TCA9548A object and give it the I2C bus
-#	tca = adafruit_tca9548a.TCA9548A(i2c)
-
-	# For each sensor, create it using the TCA9548A channel instead of the I2C object
-	tsl0 = adafruit_tsl2591.TSL2591(tca[0])
-	tsl1 = adafruit_tsl2591.TSL2591(tca[1])
-	tsl2 = adafruit_tsl2591.TSL2591(tca[2])
-	tsl3 = adafruit_tsl2591.TSL2591(tca[3])
-	tsl4 = adafruit_tsl2591.TSL2591(tca[4])
-	tsl5 = adafruit_tsl2591.TSL2591(tca[5])
-	tsl6 = adafruit_tsl2591.TSL2591(tca[6])
-	tsl7 = adafruit_tsl2591.TSL2591(tca[7])
-
-	sensor_reading = [tsl0.lux, tsl1.lux, tsl2.lux, tsl3.lux, tsl4.lux, tsl5.lux, tsl6.lux, tsl7.lux]
 	return sensor_reading
 
 if __name__ == '__main__':
@@ -31,6 +24,13 @@ if __name__ == '__main__':
 
         # Create the TCA9548A object and give it the I2C bus
 	tca = adafruit_tca9548a.TCA9548A(i2c)
-
+	sensor_reading = []
+	for i in range(8):
+		try:
+			tsl=adafruit_tsl2591.TSL2591(tca[i])
+			sensor_reading.append(tsl.lux)
+		except ValueError:
+			print('Sensor Not Found at position {}'.format(i))
+	print(sensor_reading)
 	#while True:
 	#	print(read_all_sensors(tca))
