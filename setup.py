45cf77ab0ed539248e98901229a6407e071d053e
Tianpeng Zhang <tzhang@g.harvard.edu>
2021-08-13 11:20:51 -0400

    Add talker/listener testing modules.

--- setup.py
+++ setup.py
@@ -20,6 +20,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+        'listener = light_sensor.listener:main',
+        'talker = light_sensor.talker:main'
         ],
     },
 )
